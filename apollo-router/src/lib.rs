ce76dbd890595295164341cc897b6c123a7ca7bc
smihica <smihica@gmail.com>
2024-02-15 18:11:36 +0900

    Make ConnectionInfo public

--- apollo-router/src/lib.rs
+++ apollo-router/src/lib.rs
@@ -80,6 +80,7 @@ pub mod tracer;
 mod uplink;
 
 pub use crate::axum_factory::unsupported_set_axum_router_callback;
+pub use crate::axum_factory::utils::ConnectionInfo;
 pub use crate::configuration::Configuration;
 pub use crate::configuration::ListenAddr;
 pub use crate::context::extensions::sync::ExtensionsMutex;
